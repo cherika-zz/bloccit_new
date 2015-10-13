16727e6761088720f3e69cf7826226aca79d91b4
Selene <hakobyan.sn@gmail.com>
2015-10-12 22:13:27 -0700

    crud continued

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,9 +4,28 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
+    #we create an instance variable @post, then assign it an empty post returned by Post.new
+    @post = Post.new
+  end
+
+  def create
+    # we call Post.new to create a new instance of Post
+    @post = Post.new
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+
+    if @post.save
+      flash[:notice] = "Post was saved."
+      # redirecting to posts show view
+      redirect_to @post
+    else
+      flash[:error] = "There was an error saving your post. Please try again."
+      render :new
+    end
   end
 
   def edit
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -13,29 +13,77 @@ RSpec.describe PostsController, type: :controller do
       expect(response).to have_http_status(:success)
     end
 
-#9, because our test created one post (my_post),
-# we expect index to return an array of one item. This is achieved using assigns,
-# a method in ActionController::TestCase. assigns gives the test access
- # to "instance variables assigned in the action that are available for the view".
+#9, because our test created one post (my_post), we expect index to return an array of one item. This is achieved using assigns,
+# a method in ActionController::TestCase. assigns gives the test access to "instance variables assigned in the action that are available for the view".
     it "assigns [my_post] to posts" do
       get :index
       expect(assigns(:posts)).to eq([my_post])
     end
   end
 
-  # describe "GET #show" do
-  #   it "returns http success" do
-  #     get :show
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
-  #
-  # describe "GET #new" do
-  #   it "returns http success" do
-  #     get :new
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+
+
+  #1
+  describe "GET #new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+
+    #2 we expect the PostsController#new to render the posts new view
+    it "renders the new view" do
+      get :new
+      expect(response).to render_template :new
+    end
+
+    #3 we expect the @post instance to be initialized by PostsController#new.
+    # assigns gives us access to the @post variable, assigning it to :post
+    it "instantiates @post" do
+      get :new
+      expect(assigns(:post)).not_to be_nil
+    end
+  end
+
+#4 we expect that after PostsController#create is called with parameters, the count
+# of post instances will increase by one. Create creates the fields in a post.
+  describe "POST create" do
+    it "increases the number of Post by 1" do
+      expect{post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}}
+    end
+
+    #5 we expect the newly created post to be assigned to @post
+    it "assigns the new post to @post" do
+      expect(assigns(:post)).to eq Post.last
+    end
+
+    #6 we expect to be redirected to the newly created post
+    it "redirects to the new post" do
+      post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
+      expect(response).to redirect_to Post.last
+    end
+  end
+
+# we pass {id: my_post.id} to show as a parameter. These parameters are passed to the params hash.
+  describe "GET show" do
+    it "returns http success" do
+      get :show, {id: my_post.id}
+      expect(response).to have_http_status(:success)
+    end
+
+    # we expect the response to return the show view
+    it "renders the show view" do
+      get :show, {id: my_post.id}
+      expect(response).to render_template :show
+    end
+
+    #we expect the post to equal my_post because we call show with the id of my_post
+    it "assigns my_post to @post" do
+      get :show, {id: my_post.id}
+      expect(assigns(:post)).to eq(my_post)
+    end
+  end
+
+
   #
   # describe "GET #edit" do
   #   it "returns http success" do
