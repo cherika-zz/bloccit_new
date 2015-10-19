5a3474c983050a46c65757a9f51ade7f5a088c92
Selene <hakobyan.sn@gmail.com>
2015-10-19 14:56:30 -0700

    validation and partials checkpoint

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -15,6 +15,15 @@ RSpec.describe Post, type: :model do
 
   it { should belong_to(:topic)}
 
+  # test that Post validates the presence of title, body, and topic
+  it { should validate_presence_of(:title)}
+  it { should validate_presence_of(:body)}
+  it { should validate_presence_of(:topic)}
+
+  # test that Post validates the lengths of title and body
+  it { should validate_length_of(:title).is_at_least(5)}
+  it { should validate_length_of(:body).is_at_least(20)}
+
   describe "attributes" do
 
     #2 we test whether post has an attribute named title. This tests whether
