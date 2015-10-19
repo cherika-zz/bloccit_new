e2a851d1da601c262e5154844504dcdbee6dd72a
Selene <hakobyan.sn@gmail.com>
2015-10-19 15:26:42 -0700

    validation assignment

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -6,6 +6,9 @@ RSpec.describe Topic, type: :model do
 
   it { should have_many(:posts) }
 
+  it { should validate_length_of(:name).is_at_least(5)}
+  it { should validate_length_of(:description).is_at_least(15)}
+
   describe "attributes" do
 
     #1, we confirm that a topic responds to the appropriate attributes.
