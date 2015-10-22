f030a339161815d691e4182a286128d93913b425
Selene <hakobyan.sn@gmail.com>
2015-10-22 10:17:21 -0700

    avatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,9 @@ module ApplicationHelper
       content_tag :div, capture(&block), class: 'form-group'
     end
   end
+
+  def avatar_url(user, size)
+     gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+     "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
+  end
 end
