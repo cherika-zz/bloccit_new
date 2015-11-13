2298052a4676929129ecbee80a77ddb45108b47e
Selene Hakobyan <selene@uber.com>
2015-11-12 17:09:30 -0800

    voting checkpoint

--- db/seeds.rb
+++ db/seeds.rb
@@ -59,14 +59,17 @@ topics = Topic.all
 
 # Create Posts
 50.times do
-  Post.create!(
+  post = Post.create!(
     user: users.sample,
     topic: topics.sample,
     title: RandomData.random_sentence,
     body: RandomData.random_paragraph
   )
+post.update_attribute(:created_at, rand(10.minutes..1.year).ago)
+rand(1..5).times { post.votes.create!(value: [-1, 1].sample, user: users.sample)}
 end
 posts = Post.all
+
 
 # Create Comments
 100.times do
@@ -111,3 +114,4 @@ puts "#{Comment.count} comments created"
 # puts "#{Question.count} questions created"
 # puts "#{SponsoredPost.count} sponsored posts created"
 puts "#{User.count} users created"
+puts "#{Vote.count} votes created"
